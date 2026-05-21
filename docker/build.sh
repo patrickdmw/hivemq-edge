7148b818d95ee52e6501b1c1b13bbf6bf83a4423
Patrick D'Addona <patrick.daddona@maibornwolff.de>
2026-05-21 22:41:03 +0200

    chore: include persistence module

--- docker/build.sh
+++ docker/build.sh
@@ -6,5 +6,5 @@ cd "$(dirname "${0}")" # go to the directory of this script
 cd ..
 ./gradlew -Dorg.gradle.java.home=/home/patrickd/.jdks/corretto-26.0.1 loadOciImage
 
-docker build -f docker/Dockerfile . -t acriedweeushd.azurecr.io/hivemq/hivemq-edge:2026.8-mqtt3-0.4
-docker push acriedweeushd.azurecr.io/hivemq/hivemq-edge:2026.8-mqtt3-0.4
+docker build -f docker/Dockerfile . -t acriedweeushd.azurecr.io/hivemq/hivemq-edge:2026.8-mqtt3-0.5
+docker push acriedweeushd.azurecr.io/hivemq/hivemq-edge:2026.8-mqtt3-0.5
